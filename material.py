12c84ca1cdf54480738a7feb40fdbb243bbad6f6
bindusrimudhiraj <manthribindu013@gmail.com>
2026-08-11 11:36:14 +0530

    Add user input for name and age

--- material.py
+++ material.py
@@ -1,5 +1,8 @@
 import time
-
 print("Start")
 time.sleep(2)
 print("End")
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print("Hello", name)
+print("You are", age, "years old.")
