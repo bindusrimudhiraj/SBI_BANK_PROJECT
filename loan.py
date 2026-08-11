f6b93d94c83b81476f7663193a5c1663500bd812
bindusrimudhiraj <manthribindu013@gmail.com>
2026-08-11 11:30:55 +0530

    file updated

--- loan.py
+++ loan.py
@@ -2,3 +2,8 @@ import random
 
 num = random.randint(1, 10)
 print("Random Number:", num)
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Hello", name)
+print("You are", age, "years old.")
